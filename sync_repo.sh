55befee21c6f668dfe0889acb64785902cf40759
Geidel Guerra <github.6870z@passmail.net>
2025-03-09 13:59:22 -0400

    sync

--- sync_repo.sh
+++ sync_repo.sh
@@ -10,6 +10,8 @@ cp ~/.zshrc $REPO_DIR
 rsync -a --delete ~/.config/ghostty $REPO_DIR/.config/
 rsync -a --delete ~/.config/nvim/init.lua $REPO_DIR/.config/nvim/
 rsync -a --delete ~/.config/zed $REPO_DIR/.config/
+rsync -a --delete ~/.config/Code/User/settings.json $REPO_DIR/.config/Code/User/
+rsync -a --delete ~/.config/Code/User/keybindings.json $REPO_DIR/.config/Code/User/
 
 cd $REPO_DIR
 git add . && git commit -m 'sync' && git push origin master
