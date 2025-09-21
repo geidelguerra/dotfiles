6decdbc22b921259eb676639e726a3f121b86b2c
Geidel Guerra <github.6870z@passmail.net>
2025-09-20 22:49:57 -0400

    sync

--- sync_repo.sh
+++ sync_repo.sh
@@ -2,17 +2,13 @@
 
 REPO_DIR=$HOME/projects/personal/dotfiles/
 
-cp ~/.gitconfig $REPO_DIR
-cp ~/.shell_paths $REPO_DIR
-cp ~/.tmux.conf $REPO_DIR
-cp ~/.zshrc $REPO_DIR
-
 rsync -a --delete ~/.gitconfig $REPO_DIR/
 rsync -a --delete ~/.config/ghostty $REPO_DIR/.config/
 rsync -a --delete ~/.config/nvim/init.lua $REPO_DIR/.config/nvim/
 rsync -a --delete ~/.config/zed $REPO_DIR/.config/
 rsync -a --delete ~/.config/alacritty $REPO_DIR/.config/
 rsync -a --delete ~/.config/hypr $REPO_DIR/.config/
+rsync -a --delete ~/.config/waybar $REPO_DIR/.config/
 rsync -a --delete ~/.config/Code/User/settings.json $REPO_DIR/.config/Code/User/
 rsync -a --delete ~/.config/Code/User/keybindings.json $REPO_DIR/.config/Code/User/
 
