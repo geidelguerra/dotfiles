2ec77a41f1e7ecaad03d44225a2e512c09681f2c
Geidel Guerra <github.6870z@passmail.net>
2025-10-02 11:12:22 -0400

    sync

--- sync_repo.sh
+++ sync_repo.sh
@@ -9,8 +9,7 @@ rsync -a --delete ~/.config/zed $REPO_DIR/.config/
 rsync -a --delete ~/.config/alacritty $REPO_DIR/.config/
 rsync -a --delete ~/.config/hypr $REPO_DIR/.config/
 rsync -a --delete ~/.config/waybar $REPO_DIR/.config/
-rsync -a --delete ~/.config/Code/User/settings.json $REPO_DIR/.config/Code/User/
-rsync -a --delete ~/.config/Code/User/keybindings.json $REPO_DIR/.config/Code/User/
+rsync -a --delete ~/.config/mako $REPO_DIR/.config/
 
 cd $REPO_DIR
 git add . && git commit -m 'sync' && git push origin master
