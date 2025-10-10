1fab19d8d897ca8b74379be3d1a0079539662d7b
Geidel Guerra <github.6870z@passmail.net>
2025-10-09 23:26:38 -0400

    wip

--- setup.sh
+++ setup.sh
@@ -13,3 +13,13 @@ fi
 # Create a symbolic link to sync.sh as syncdotfiles
 ln -sf "$REPO_DIR/sync.sh" /usr/local/bin/syncdotfiles
 chmod +x /usr/local/bin/syncdotfiles
+
+# Add Chaotic-AUR repository
+if ! grep -q "\[chaotic-aur\]" /etc/pacman.conf; then
+    pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+    pacman-key --lsign-key 3056513887B78AEB
+    pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+    pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+    echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+    packman -Syu --noconfirm
+fi
